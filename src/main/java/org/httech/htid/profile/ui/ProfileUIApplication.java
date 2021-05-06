bbee48392090ac5b81963f436255b493305d311a
dinh.phan <congdinhphan@gmail.com>
2021-05-06 10:01:32 +0700

    remove unused

--- src/main/java/org/httech/htid/profile/ui/ProfileUIApplication.java
+++ src/main/java/org/httech/htid/profile/ui/ProfileUIApplication.java
@@ -2,7 +2,6 @@ package org.httech.htid.profile.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class ProfileUIApplication {
